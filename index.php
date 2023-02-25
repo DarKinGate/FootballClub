579e67c5c65132d9d1778d2c776c2ca0eee22800
DarKinGate <dark8d8ice@gmail.com>
2023-02-25 20:44:22 +0400

    First Version

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="This is a description of the Football Club page. The Football Club is a professional soccer team based in Mannheim, Germany. The club was founded in 1995 and has won 28 championships.">
+  <meta name="keywords" content="Football Club, soccer, championships">
+
+  <title>FOOTBALL  CLUB</title>
+  <!-- Other meta tags, stylesheets, and scripts can be added here -->
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <!-- Header content goes here -->
+    <a href="index.php">
+      <img src="logo.svg" id="logo" alt="Football Club Logo">
+    <h1>
+      Football Club</a>
+    </h1>
+  </header>
+  <nav>
+    <!-- Navigation content goes here -->
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+    <a href="#" id="login">Login</a>
+  </nav>
+  <main>
+    <!-- Main content goes here -->
+    Main Content
+  </main>
+  <aside>
+    <!-- A Side Content goes here -->
+    Side Content
+  </aside>
+  <footer>
+    <!-- Footer content goes here -->
+    Footer with copyright and links
+  </footer>
+</body>
+</html>
