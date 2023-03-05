8c296308bef154fb982e0ab91f6989750e744e2c
DarKinGate <dark8d8ice@gmail.com>
2023-03-05 23:05:28 +0400

    user area

--- Partials/_user_main_content.php
+++ Partials/_user_main_content.php
@@ -4,11 +4,11 @@ $sql = "SELECT Email, Name, Last_Name, Position, Notes FROM fb_players where Ema
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 ?>
-<fieldset>
+<fieldset id="user_display">
     <legend>Player Data</legend>
     <label>Firstname</label><input type="text" value="<?php echo($row['Name']); ?>" disabled></input>
     <label>Lastname</label><input type="text" value="<?php echo($row['Last_Name']); ?>" disabled></input>
     <label>Email</label><input type="text" value="<?php echo($row['Email']); ?>" disabled></input>
     <label>Position</label><input type="text" value="<?php echo($row['Position']); ?>" disabled></input>
-    <label>Notes</label><input type="text-area" value="<?php echo($row['Notes']); ?>" disabled></input>
+    <label>Notes</label><textarea disabled><?php echo($row['Notes']); ?></textarea>
 </fieldset>
