3b22efa35f95c88e4b05905663476975610fd018
DarKinGate <dark8d8ice@gmail.com>
2023-04-05 23:09:27 +0400

    links fix

--- Partials/_main_content.php
+++ Partials/_main_content.php
@@ -1,6 +1,14 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
 $host = $_SERVER['DOCUMENT_ROOT'];
-if($url == '/gallery.php') include($host.'/Partials/Handlers/_gallery_page_handler.php');
-if($url == '/links.php') include($host.'/Partials/Handlers/_links_handler.php');
-if($url == '/') include($host.'/Partials/Handlers/_home_page_handler.php');
+
+if ($url === '/gallery.php') {
+  require($_SERVER['DOCUMENT_ROOT'] . '/Partials/Handlers/_gallery_page_handler.php');
+} elseif ($url === '/links.php') {
+  require($_SERVER['DOCUMENT_ROOT'] . '/Partials/Handlers/_links_handler.php');
+} elseif ($url === '/') {
+  require($_SERVER['DOCUMENT_ROOT'] . '/Partials/Handlers/_home_page_handler.php');
+} else {
+  // Handle unknown requests
+  echo "Page not found";
+}
